a328a7cbd85bfc3c0a2a1679af8192a1c6c7c03d
Abubokkor98 <mail.abubokkor@gmail.com>
2024-09-21 19:12:00 +0600

    add event listener for saving button

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-// add event listener for calculate button
+// getting all the value
 
+// add event listener for calculate button
 const calCulateButton = document.getElementById('calculate').addEventListener('click', function (){
 
     const income  = parseFloat(document.getElementById('income').value);
     const software  = parseFloat(document.getElementById('software').value);
     const courses  = parseFloat(document.getElementById('courses').value);;
     const internet = parseFloat(document.getElementById('internet').value);
+
+
+
 
     const totalExpenses = software + courses + internet;
     const balance = income - totalExpenses;
@@ -19,4 +23,31 @@ const calCulateButton = document.getElementById('calculate').addEventListener('c
 
     const result = document.getElementById('results');
     result.classList.remove('hidden');
+})
+
+// add event listener for saving button
+const calculateSavingButton = document.getElementById('calculate-savings').addEventListener('click',function(){
+
+    const savingPercentage = parseFloat(document.getElementById('savings').value);
+    console.log(savingPercentage);
+
+    const income  = parseFloat(document.getElementById('income').value);
+    const software  = parseFloat(document.getElementById('software').value);
+    const courses  = parseFloat(document.getElementById('courses').value);;
+    const internet = parseFloat(document.getElementById('internet').value);
+
+    
+    const totalExpenses = software + courses + internet;
+    const balance = income - totalExpenses;
+
+    const savingAmount = (savingPercentage * balance) / 100;
+    console.log(savingAmount);
+
+    const savingElement = document.getElementById('savings-amount');
+    savingElement.innerText = savingAmount.toFixed(2);
+
+    const remainingBalance = balance - savingAmount;
+    const remainingBalanceElement = document.getElementById('remaining-balance');
+    remainingBalanceElement.innerText = remainingBalance.toFixed(2);
+
 })
